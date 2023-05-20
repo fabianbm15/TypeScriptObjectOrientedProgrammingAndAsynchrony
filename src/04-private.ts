ce7337f51b18ad7934a7ac0f3575007780931e90
Carlos Fabián Melgarejo Agudelo <cmelgarejobm@gmail.com>
2023-05-20 09:04:02 -0500

    Private Access

---
+++ src/04-private.ts
@@ -0,0 +1,52 @@
+export class MyDate {
+  private year: number;
+  private month: number;
+  private day: number;
+
+  constructor(year: number, month: number, day: number) {
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+
+  printFormat(): string {
+    const day = this.addPadding(this.day);
+    const month = this.addPadding(this.month);
+
+    return `${day}/${month}/${this.year}`;
+  }
+
+  private addPadding(value: number) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+    return `${value}`;
+  }
+  add(amount: number, type: 'days' | 'months' | 'years') {
+    if (type === 'days') {
+      this.day += amount;
+    }
+    if (type === 'months') {
+      this.month += amount;
+    }
+    if (type === 'years') {
+      this.year += amount;
+    }
+  }
+
+  getDay() {
+    return this.day;
+  }
+  getMonth() {
+    return this.month;
+  }
+  getYear() {
+    return this.year;
+  }
+}
+
+const myDate = new MyDate(1993, 7, 9);
+console.log('Fecha', myDate.printFormat());
+console.log('Día', myDate.getDay());
+console.log('Mes', myDate.getMonth());
+console.log('Año', myDate.getYear());
